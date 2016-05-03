01f4203afa4484847f1e525991c565f740ab7934
vibesharing <buret.hadrien@orange.fr>
2016-05-03 11:54:50 +0200

    dev ready for round 2

--- front/www/js/app.js
+++ front/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -46,6 +46,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       'tab-profile': {
         templateUrl: 'templates/tab-profile.html',
         controller: 'ProfileCtrl'
+      }
+    }
+  })
+  .state('tab.list', {
+    url: '/list',
+    views: {
+      'tab-list': {
+        templateUrl: 'templates/tab-list.html',
+        controller: 'listCtrl'
       }
     }
   })
--- front/www/js/controllers/listCtrl.js
+++ front/www/js/controllers/listCtrl.js
@@ -1,6 +1,6 @@
-listCtrl = function($scope, $state, $ngCordova, $http){
+function listCtrl($scope, $state, $http){
   $http.get('http://localhost:8000/profiles').then(function(res){
     $scope.list = res.data;
   });
 
-};
+}
